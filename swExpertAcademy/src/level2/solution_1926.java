a264f905cede7ccbce2cc876461947c651271718
DESKTOP-7FFOCP5\SKN <ppp9026@naver.com>
2021-08-04 15:52:08 +0900

    369 수정..

--- swExpertAcademy/src/level2/solution_1926.java
+++ swExpertAcademy/src/level2/solution_1926.java
@@ -7,35 +7,28 @@ public class solution_1926 {
         Scanner sc = new Scanner(System.in);
         int T;
         T = sc.nextInt();
-		/*
-		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-		*/
+
         String result = "";
         for(int i = 1; i <= T; i++){
-            //result += (i > 1 ? " " : "") + numberProc(String.valueOf(i));
-            result += (i > 1 ? " " : "")
-                    + String.valueOf(i)
-                        .replaceAll("3", "-")
-                        .replaceAll("6", "-")
-                        .replaceAll("9", "-");
+            result += (i > 1 ? " " : "") + numberProc(String.valueOf(i));
         }
 
         System.out.println(result);
         sc.close();
     }
 
-    //3, 33, 39
-    //재귀..?
+    //3, 33, 39  312 332 313 300 333
     public static String numberProc(String num){
-        //base 케이스
-        if(!num.contains("3") && !num.contains("6") && !num.contains("9")){
-            return num;
+        if(num.contains("3") || num.contains("6") || num.contains("9")){
+            char[] charArray = num.toCharArray();
+            num = "";
+            for(var i=0; i<charArray.length; i++){
+                if(charArray[i] == '3' || charArray[i] == '6' || charArray[i] == '9') {
+                    num += "-";
+                }
+            }
         }
 
-        num = num.replace("3", "-");
-        num = num.replace("6", "-");
-        num = num.replace("9", "-");
-
-        return numberProc(num);
+        return num;
     }
 }
