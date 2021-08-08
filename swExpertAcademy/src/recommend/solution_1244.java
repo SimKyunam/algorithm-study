cd810b45ab7644edaa6bf9a8cef7ef874036dad2
kyunam Sim <ppp9026@naver.com>
2021-08-08 16:58:30 +0900

    1244 [S/W 문제해결 응용] 2일차 - 최대 상금 -> 정답 보고 수정. dfs 이해 후 다시보기

--- swExpertAcademy/src/recommend/solution_1244.java
+++ swExpertAcademy/src/recommend/solution_1244.java
@@ -3,52 +3,72 @@ package recommend;
 import java.util.*;
 
 public class solution_1244 {
+//    5
+//    123 1     321
+//    2737 1    7732
+//    32888 2   88832
+//    4321 1
+//    1129 2
+    static int T, N, max;
+    static char[] nums;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int T;
         T = sc.nextInt();
 
-        for(int i = 1; i <= T; i++){
-            String number = sc.next();
-            int changeCnt = sc.nextInt();
+        for (int t = 1; t <= T; t++) {
+            String s = sc.next();
+            N = sc.nextInt();
+            nums = s.toCharArray();
+            max = 0;
+            StringBuilder ssb = new StringBuilder();
 
-            for(int j=0; j<changeCnt; j++){
-                number = changeNumber(number);
+            if (s.length() <= N) {
+                // 이 상황이면, 무조건 최대 값을 만들 수 있음
+                for (int i = 0; i < nums.length; i++) {
+                    int maxIdx = 0;
+                    for (int j = 0; j < nums.length; j++) {
+                        if (nums[j] > nums[maxIdx]) {
+                            maxIdx = j;
+                        }
+                    }
+                    ssb.append(nums[maxIdx]);
+                    nums[maxIdx] = 0;
+                }
+
+                max = Integer.parseInt(ssb.toString());
             }
-
-            System.out.println("#"+i + " " + number);
+            else {
+                dfs(0);
+            }
+            System.out.println("#" + t + " " + max);
         }
         sc.close();
     }
 
-    public static String changeNumber(String number){
-        List<Integer> numberList = new ArrayList<>();
-
-        char[] numberCharArray = null;
-        char temp;
-
-        for(int i=0; i<number.length(); i++){
-            numberCharArray = number.toCharArray();
-
-            for(int j=0; j<number.length(); j++){
-                temp = numberCharArray[i];
-                numberCharArray[i] = numberCharArray[j];
-                numberCharArray[j] = temp;
-
-                int changeNumber = Integer.parseInt(String.valueOf(numberCharArray));
-                if(numberList.indexOf(changeNumber) == -1){
-                    numberList.add(changeNumber);
-                }
+    private static void dfs(int cnt) {
+        // 기저 조건, 종료 조건
+        if (cnt == N) {
+            StringBuilder sb = new StringBuilder();
+            for (char c : nums) {
+                sb.append(c);
             }
+            max = Math.max(max, Integer.parseInt(sb.toString()));
+            return;
         }
 
-        numberList.remove(numberList.indexOf(Integer.parseInt(number)));
-        numberList.sort(Comparator.reverseOrder());
-
-        return String.valueOf(numberList.get(0));
+        // 반복
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                char temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                dfs(cnt + 1);
+                temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
     }
-
-
 
 }
