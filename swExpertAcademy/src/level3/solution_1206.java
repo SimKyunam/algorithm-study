9c518a7dec61a52a0e06ac795f12b44628bdc8e4
kyunam Sim <ppp9026@naver.com>
2021-08-28 22:43:37 +0900

    - 1206. [S/W 문제해결 기본] 1일차 - View

--- swExpertAcademy/src/level3/solution_1206.java
+++ swExpertAcademy/src/level3/solution_1206.java
@@ -26,28 +26,29 @@ public class solution_1206 {
             maxIndex = 0;
             standard = buildings[i];
 
-            //max 구하기
+            //양옆 2개의 빌딩 중 가장 높은 높이 구하기 (max)
             for(int j = 1; j < 3; j++){
-                if(buildings[i+j] > max){
+                if(buildings[i+j] > max){ //기준빌딩의 오른쪽 2칸
                     max = buildings[i+j];
-                    if(buildings[i] < buildings[i+j]){
+                    if(buildings[i] < buildings[i+j]){ //기준 빌딩보다 높은 빌딩이 존재하는 경우 그 빌딩으로 이동
                         maxIndex = i+j;
                     }
                 }
-                if(buildings[i-j] > max){
+                if(buildings[i-j] > max){ //기준빌딩의 왼쪽 2칸
                     max = buildings[i-j];
                 }
             }
 
+
             if(standard > buildings[i-1]
                     && standard > buildings[i-2]
                     && standard > buildings[i+1]
-                    && standard > buildings[i+2]) {
+                    && standard > buildings[i+2]) { //기준 빌딩이 가장 높은 경우
                 totSum += standard - max;
                 i += 2;
             }else{
-                if(maxIndex > 0)
-                    i = maxIndex-1;
+                if(maxIndex > 0) //현재 기준점에 다음 2칸 중 높은 빌딩이 있는 경우
+                    i = maxIndex-1; //반복문에 시작을 가장 높은 maxIndex 위치로 이동
             }
         }
 
