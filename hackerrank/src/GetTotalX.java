dac4be089f5b5bc9f26a2993332dbce97e992830
DESKTOP-851JB71\Sim <ppp9026@naver.com>
2022-01-28 23:02:05 +0900

    HackerRank

--- hackerrank/src/GetTotalX.java
+++ hackerrank/src/GetTotalX.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -32,6 +34,43 @@ public class GetTotalX {
 
     public static int getTotalX(List<Integer> a, List<Integer> b) {
         // Write your code here
-        return 0;
+        int resultCnt = 0;
+        Integer gcdValue = a.stream().reduce(GetTotalX::gcd).orElse(0);
+        Integer maxValue = a.stream().reduce(1, ((integer, integer2) -> integer * integer2)) / gcdValue;
+
+        b.sort(Comparator.naturalOrder());
+        Integer minValue = b.stream().findFirst().orElse(0);
+
+        List<Integer> gcdArray = new ArrayList<>();
+        if(maxValue > 1 || maxValue < minValue) {
+            int i=1;
+            int breakValue = 0;
+            while (breakValue < minValue) {
+                breakValue = maxValue * i;
+                gcdArray.add(breakValue);
+                i++;
+            }
+        }
+
+        for(Integer gcdInteger : gcdArray) {
+            int successCnt = 0;
+            for(Integer integer : b) {
+                if(gcdInteger >= integer && gcdInteger % integer == 0) successCnt++;
+                else if(gcdInteger <= integer && integer % gcdInteger == 0) successCnt++;
+            }
+
+            if(successCnt == b.size()) {
+                resultCnt++;
+            }
+        }
+
+        return resultCnt;
+    }
+
+    public static int gcd(int a, int b) {
+        if(b == 0) {
+            return a;
+        }
+        return gcd(a, a % b);
     }
 }
