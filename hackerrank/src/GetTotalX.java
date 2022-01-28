a2cc990ff037f0b9706d877068e5f74526fe79ec
DESKTOP-851JB71\Sim <ppp9026@naver.com>
2022-01-28 19:22:34 +0900

    HackerRank

---
+++ hackerrank/src/GetTotalX.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class GetTotalX {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> brr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int total = getTotalX(arr, brr);
+        System.out.println(total);
+
+        bufferedReader.close();
+    }
+
+    public static int getTotalX(List<Integer> a, List<Integer> b) {
+        // Write your code here
+        return 0;
+    }
+}
