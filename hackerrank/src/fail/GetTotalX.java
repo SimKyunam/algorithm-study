f619b069c4248746e82e55a46948cc108677918f
DESKTOP-851JB71\Sim <ppp9026@naver.com>
2022-02-01 11:15:39 +0900

    HackerRank

--- hackerrank/src/fail/GetTotalX.java
+++ hackerrank/src/fail/GetTotalX.java
@@ -34,6 +34,8 @@ public class GetTotalX {
         bufferedReader.close();
     }
 
+    // 1부터 100까지에서 A(나머지를 구하는 기준이 되는 값)과, B(구하는 값)을 기준으로 구하기..
+    // 최대공약수, 최소공배수로 생각하다 틀림.. (유클리드 호제법도 알아두기)
     public static int getTotalX(List<Integer> a, List<Integer> b) {
         // Write your code here
         int resultCnt = 0;
