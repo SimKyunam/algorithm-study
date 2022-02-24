f47f40b738b20439cc419cb9c98b84c90f7c2ba9
DESKTOP-851JB71\Sim <ppp9026@naver.com>
2022-02-24 21:07:31 +0900

    Hacker Rank

--- hackerrank/src/ing/FormingMagicSquare.java
+++ hackerrank/src/ing/FormingMagicSquare.java
@@ -36,6 +36,11 @@ public class FormingMagicSquare {
     }
 
     public static int formingMagicSquare(List<List<Integer>> s) {
+        int[][][] magicSquare = {{{8, 1, 6}, {3, 5, 7}, {4, 9, 2}}, {{4, 3, 8}, {9, 5, 1}, {2, 7, 6}},
+                {{2, 9, 4}, {7, 5, 3}, {6, 1, 8}}, {{6, 7, 2}, {1, 5, 9}, {8, 3, 4}},
+                {{6, 1, 8}, {7, 5, 3}, {2, 9, 4}}, {{8, 3, 4}, {1, 5, 9}, {6, 7, 2}},
+                {{4, 9, 2}, {3, 5, 7}, {8, 1, 6}}, {{2, 7, 6}, {9, 5, 1}, {4, 3, 8}}};
+
         // Write your code here
         List<Integer> dupleList = new ArrayList<>(); //중복된 값
         List<Integer> existsList = new ArrayList<>(); //존재 값
